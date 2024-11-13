e581c187b8148d066c660fa2f20075586dffa97e
Arthur <arthurgonclvs@gmail.com>
2024-11-13 12:24:05 -0300

    new functionalities

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,8 +1,7 @@
-
-
 class Game{
     static player = 0;
     static #characters = ["X", "O"];
+    static tries = 0
     
     static addCharacter(square, player){
         const squares = document.querySelectorAll(".square-space");
@@ -10,9 +9,21 @@ class Game{
         if(squares[square].innerText == ""){
             const htmlPlayer = `<span class="played">${this.#characters[player]}</span>`;
             squares[square].insertAdjacentHTML("beforeend", htmlPlayer);
-            if(this.checkWin()){
-                return;
+
+            if(this.checkWin(this.player)){
+                document.querySelector(".game-ended").classList.remove("invisible")
+                document.querySelector(".player").innerText = this.player + 1;
+                return
             }
+
+            if(!this.checkWin(this.player)){
+                this.tries++
+                if(this.tries == 9){
+                    document.querySelector(".game-ended").classList.remove("invisible")
+                    document.querySelector(".game-ended").innerText = "It's a draw!";
+                }
+            }
+
             this.changePlayer();
             return;
         }
@@ -27,15 +38,27 @@ class Game{
         this.player = 0;
     }
 
-    static checkWin(){
+    static checkWin(player){
         const wins = [
-            [0, 1, 2], [0, 3, 6], [0, 4, 8], 
-            [1, 4, 7], 
-            [2, 4, 6], [2, 5, 8]]
-        
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 6]
+        ]
         const squares = document.querySelectorAll(".square-space");
+       
+        return wins.some(combination => {
+            return combination.every(index => {
+                return squares[index].innerText == this.#characters[player];
+            })
+        })
     }
+
+    static restartGame(){
+        
+    }
+
 }
+
 
 document.querySelectorAll(".square-space").forEach((elm, ind) => {
     elm.addEventListener("click", () => {
