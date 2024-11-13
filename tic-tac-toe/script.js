649b7791e4ffc9b64f7b52decb69206059cdad71
Arch <arthurgonclvs@gmail.com>
2024-11-12 22:25:04 -0300

    structuring check win

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,13 +1,18 @@
-const squares = document.querySelectorAll(".square-space");
+
 
 class Game{
     static player = 0;
     static #characters = ["X", "O"];
-
+    
     static addCharacter(square, player){
+        const squares = document.querySelectorAll(".square-space");
+
         if(squares[square].innerText == ""){
             const htmlPlayer = `<span class="played">${this.#characters[player]}</span>`;
             squares[square].insertAdjacentHTML("beforeend", htmlPlayer);
+            if(this.checkWin()){
+                return;
+            }
             this.changePlayer();
             return;
         }
@@ -24,13 +29,15 @@ class Game{
 
     static checkWin(){
         const wins = [
-            [0, 1, 2], [0, 3, 6], 
+            [0, 1, 2], [0, 3, 6], [0, 4, 8], 
             [1, 4, 7], 
             [2, 4, 6], [2, 5, 8]]
+        
+        const squares = document.querySelectorAll(".square-space");
     }
 }
 
-squares.forEach((elm, ind) => {
+document.querySelectorAll(".square-space").forEach((elm, ind) => {
     elm.addEventListener("click", () => {
         Game.addCharacter(ind, Game.player);
     })
